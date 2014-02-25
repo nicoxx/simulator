affdb0c40de1791ef46268968f562859fa8db06c
vgrosbois <vincent.grosbois@gmail.com>
2014-02-25 01:21:17 +0100

    correc bug.... mais je trouve tjs pas 99%

--- TD_Dark_Pool/TD_dark_pool.py
+++ TD_Dark_Pool/TD_dark_pool.py
@@ -6,6 +6,7 @@ Created on Mon Feb 24 15:10:30 2014
 """
 
 import random
+from math import * 
 import matplotlib.pyplot as plt
 
 
@@ -16,10 +17,9 @@ Var_D = list()
 
 rho = list()
 for i in range(N):
-    E_D.append(i + 1)
-    Var_D.append(1)
+    E_D.append(1.*(i + 1))
+    Var_D.append(1.)
     rho.append(1./((i+2)*(i+2)))
-
 
 
 rho[0] = 0.01
@@ -27,38 +27,34 @@ rho[1] = 0.03
 rho[2] = 0.05
 
 
-print('E_D')
+print('Esperance D:')
 print(E_D)
-print('Var_D')
+print('Variance D:')
 print(Var_D)
 
-Var_V = 1
+Var_V = 1.
 E_V = sum(E_D)*3/2
 
-
-print('E_V')
+print('Esperance V:')
 print(E_V)
-print('Var_V')
+print('Variance V:')
 print(Var_V)
 
-sigma_V =  sqrt( log( 1  + Var_V / E_V*E_V ) ) 
-mu_V = log (E_V) -  0.5*  log( 1  + Var_V / E_V*E_V )
-
+sigma_V =  sqrt( log( 1  + Var_V / (E_V*E_V) ) ) 
+mu_V = log (E_V) -  0.5*  log( 1  + Var_V / (E_V*E_V) )
 
 
 sigma_D = list()
 mu_D = list()
-
-
 for i in range(N):
-    sigma_D.append( sqrt( log( 1  + Var_D[i] / E_D[i]*E_D[i] ) )  )
-    mu_D.append( log (E_D[i]) -  0.5*  log( 1  + Var_D[i] / E_D[i]*E_D[i] ) )    
+    sigma_D.append( sqrt(log ( 1.  + Var_D[i] / (E_D[i]*E_D[i]) ) )  )
+    mu_D.append( log (E_D[i]) -  0.5*  log( 1  + Var_D[i] / (E_D[i]*E_D[i]) ) )    
     
    
+#print(sigma_D) #pour le debug...
 
 
-
-print "rho :"
+print "Rho :"
 print rho
 
 r = list()
@@ -73,41 +69,40 @@ for n in range(100000):
     
     D = list()
     for i in range(N):
-        D.append(random.lognormvariate(0, sigma_D[i])) 
-    
+        D.append(random.lognormvariate(mu_D[i], sigma_D[i])) 
     
     indic = list()
     for i in range(N):
         if r[i]*V < D[i]:
-            indic.append(1)
+            indic.append(1.)
         else:
-            indic.append(0)
-    
-    
-    S = 0
+            indic.append(0.)
+      
+    S = 0.
     for i in range(N):
-        S = S + rho[i]*indic[i]
-        
+        S = S + rho[i]*indic[i]  
     S = S/N
-    
     
     
     for i in range(N):
         r[i] = r[i] + (1./(n+1))*V*(rho[i]*indic[i] - S)
 
-print "r estimé"
+    #print r
+    
+print "estimation r :"
 print r
 
-print sum(r)
+#partie test 
 
 C1_tot = 0
 C2_tot = 0
-test_n = 20000
+test_n = 10000
 
 C1_list = list()
 C2_list = list()
 perf_list = list()
-
+ratio_list = list()
+ratio = 0
 for n in range(test_n):
     V = random.lognormvariate(mu_V, sigma_V)
     
@@ -115,8 +110,8 @@ for n in range(test_n):
     for i in range(N):
         D.append(random.lognormvariate(mu_D[i], sigma_D[i]))
         
-    C1 = 0
-    C2 = 0
+    C1 = 0.
+    C2 = 0.
     
     dark_pools = list()  
     
@@ -147,6 +142,9 @@ for n in range(test_n):
     C2_tot += C2 / V
     C1_list.append(C1_tot / (n+1))
     C2_list.append(C2_tot / (n+1))
+    
+    ratio += C1 / C2
+    ratio_list.append(ratio / (n+1))
     perf_list.append( C1_tot / C2_tot )
     
 
@@ -167,4 +165,5 @@ print(C1_tot / C2_tot)
 plt.plot(C1_list)
 plt.plot(C2_list)
 plt.figure()
+plt.plot(ratio_list)
 plt.plot(perf_list)
